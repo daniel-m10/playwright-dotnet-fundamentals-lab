de9e330c3830a2e883e941e440c55781e091cdec
daniel-m10 <daniel.montecinos.pinto@gmail.com>
2026-06-22 20:17:25 -0400

    feat: add PlaywrightHomePage page object and reorganize tests into Pages/Tests folders

---
+++ PlaywrightFundamentalsLab.Tests/Tests/SmokeTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright.NUnit;
+using PlaywrightFundamentalsLab.Tests.Pages;
+
+namespace PlaywrightFundamentalsLab.Tests.Tests;
+
+[TestFixture]
+public class SmokeTest : PageTest
+{
+    private PlaywrightHomePage _homePage = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        _homePage = new PlaywrightHomePage(Page);
+    }
+    
+    [Test]
+    public async Task ShouldLoadPlaywrightHomepage()
+    {
+        await _homePage.GotoAsync();
+        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+    }
+
+    [Test]
+    public async Task ShouldShowGetStartedLink()
+    {
+        await _homePage.GotoAsync();
+        Assert.That(await _homePage.IsGetStartedLinkVisibleAsync(), Is.True);
+    }
+}
